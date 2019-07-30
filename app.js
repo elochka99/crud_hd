ec3ac81d2edbfa16691130825a9475eb78e781bd
elochka99 <elochka150899@gmail.com>
2019-07-31 02:43:25 +0300

    commit

---
+++ app.js
@@ -0,0 +1,46 @@
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let mongoose = require('mongoose');
+
+let indexRouter = require('./routes/index');
+
+let app = express();
+
+// mongoose.connect('mongodb://root:edhemnetqx71198@ds139534.mlab.com:39534/heroku_dhlwt4cm', { useNewUrlParser: true });
+mongoose.connect('mongodb://root:qwerty1234567@ds257507.mlab.com:57507/heroku_js5123pp', { useNewUrlParser: true });
+mongoose.connection.on('error', function() {
+    console.error('MongoDB Connection Error. Make sure MongoDB is running.');
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
